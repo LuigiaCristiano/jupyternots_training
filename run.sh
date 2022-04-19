5dec5ad75bcdf39c5505b3395b932d739a1a12da
Foerster, T <thomas.foerster@hzdr.de>
2022-04-19 17:04:16 +0200

    Update run.sh script

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 conda install curl
 # Install poetry
 curl -sSL https://install.python-poetry.org | python3 -
+poetry install --no-dev
 poetry shell
 python -m ipykernel install --user --name=rdm-training
-exit
+exit 0
 
 # jupyter kernelspec remove testenv
