f73f8bf3e793758d6b56d3887379aa5f14ea7a3f
Cristiano, Luigia <luigia.cristiano@helmholtz-berlin.de>
2022-05-18 10:13:54 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Install curl
 #conda update -n base conda
-conda install curl
+#conda install curl
 # Install poetry
-curl -sSL https://install.python-poetry.org | python3 -
+#curl -sSL https://install.python-poetry.org | python3 -
+pip install --user poetry
 export PATH="/home/jovyan/.local/bin:$PATH"
-poetry install --no-dev
+#poetry install --no-dev
 poetry run ipython kernel install --user --name=rdm-training
