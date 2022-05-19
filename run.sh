1618ff0a209d11b695d96f654cb467e74b634f77
Foerster, T <thomas.foerster@hzdr.de>
2022-05-19 15:39:36 +0200

    Simplified the run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Install curl
-#conda update -n base conda
-conda install curl
-# Install poetry
-curl -sSL https://install.python-poetry.org | python3 -
-export PATH="/home/jovyan/.local/bin:$PATH"
+pip install poetry
 poetry install --no-dev
 poetry run ipython kernel install --user --name=rdm-training
+
